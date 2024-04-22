54fd8eecf6850229e3869f4be0a83eabb95d5dd1
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 22:58:01 -0400

    Added 4th and 5th enemies
    The 4th uses purely symbols and the 5th/boss uses all previous symbols for the toughest challenge

---
+++ Enemies/Boss.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemp.Enemies
+{
+    internal class Boss : Enemy
+    {
+
+        static int WordLength;
+        static double time = 15;
+        private string options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+        internal override void sethp(int level, int round)
+        {
+            hp = ((level + (5 * (round - 1))) * 3);
+            hp = 1;
+        }
+        internal override void setword()
+        {
+            word = string.Empty;
+            Random random = new Random();
+            for (int i = 0; i < WordLength; i++)
+            {
+                word += options[random.Next(0, options.Length)];
+            }
+            ShowWord(time);
+        }
+        internal override void setwordLength(int level, int round)
+        {
+            WordLength = level + (5 * (round - 1));
+        }
+    }
+}
