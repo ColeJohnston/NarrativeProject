2569a8d02463b6099899cddae0b5ab16a1fdc3ec
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 22:58:46 -0400

    Added 2 rooms
    These 2 rooms store the first 2 enemies

---
+++ Rooms/CaseRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemp.Rooms
+{
+    internal class CaseRoom : Room
+    {
+        internal override void introduction(bool[] beatR, bool[] beatL)
+        {
+            if (beatL[1])
+            {
+                Console.WriteLine("The beast is still lying dead on the floor with limbs removed. Venture on while you still can.");
+            }
+            else if (!beatR[0])
+            {
+                Console.WriteLine("\noingus boingus we are here");
+            }
+            else if (!beatR[1])
+            {
+                //second time in room 2
+            }
+            else
+            {
+                //final time in room 2
+            }
+            Fighting.Fight();
+        }
+    }
+}
---
+++ Rooms/CharacterRoom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProjectTemp.Rooms
+{
+    internal class CharacterRoom : Room
+    {
+        internal override void introduction(bool[] beatR, bool[] beatL)
+        {
+            if (beatL[0])
+            {
+                Console.WriteLine("The beast is still lying dead on the floor with limbs removed. Venture on while you still can.");
+            }
+            else if (!beatR[0])
+            {
+                Console.WriteLine("" +
+                    "“You approach the room, it seems quiet. " +
+                    "\nThere are symbols on the walls, they look like lowercase letters. " +
+                    "\nSuddenly a beast appears, yet weirdly quiet but definitely threatening you. " +
+                    "\nYou remember your dad’s note. The beast begins to speak");
+            }
+            else if (!beatR[1])
+            {
+                Console.WriteLine("You find yourself once again in the room with the first set of cryptic symbols. " +
+                    "\nDespite its familiarity, a sense of unease grips you as you tread the well-worn path." +
+                    "\n IT appears and begins to speak again");
+            }
+            else
+            {
+                Console.WriteLine("You arrive again, for the final time. Knowing this beast will be even tougher this time." +
+                    "\n it appears again, just as it did twice before, and begins to speak");
+            }
+            Fighting.Fight();
+        }
+    }
+}
