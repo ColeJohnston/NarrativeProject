1939537a770dfc3fb107eb0a585c57c41cfd8b94
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 22:55:49 -0400

    Added second enemy
    Includes its Hp and time limit as well as word length

---
+++ Enemies/CaseEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemp.Enemies
+{
+    internal class CaseEnemy : Enemy
+    {
+        static int WordLength;
+        static double time = 7.5;
+        private string options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        internal override void sethp(int level, int round)
+        {
+            hp = ((level + (5 * (round - 1))) * 3);
+        }
+        internal override void setword()
+        {
+            word = string.Empty;
+            Random random = new Random();
+            for (int i = 0; i < WordLength; i++)
+            {
+                word += options[random.Next(0, options.Length)];
+            }
+            ShowWord(time);
+        }
+        internal override void setwordLength(int level, int round)
+        {
+            WordLength = level + (5 * (round-1));
+        }
+    }
+}
