30e323a068a4cd2f88e051327bd0bfa3b60fd672
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 23:07:25 -0400

    Added more system threading
    Had more errors with System threading that i fixed here

--- Player.cs
+++ Player.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO.Pipes;
 using System.Linq;
+using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
 
