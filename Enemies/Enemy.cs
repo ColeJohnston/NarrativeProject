fccd9f55d529de8720ef61dd39ddd56f86be85a1
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 22:48:02 -0400

    Added templates
    Added templates for Enemy and Room

---
+++ Enemies/Enemy.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ProjectTemp.Enemies
+{
+    internal abstract class Enemy
+    {
+        static int dealt = 0;
+        static int recieved = 0;
+        public static int hp;
+        public static int nap = 0;
+        string attempt = string.Empty;
+        public static string word = string.Empty;
+        internal abstract void sethp(int level, int round);
+        internal abstract void setword();
+        internal abstract void setwordLength(int level, int round);
+        public void ShowWord(double time)
+        {
+            Game.showHp();
+            Console.WriteLine(word);
+            Thread.Sleep((int)(time*1000 * (Math.Pow(0.75,nap))));
+            Console.Clear();
+            Game.showHp();
+            attempt = Console.ReadLine();
+            if (attempt == "i" && Game.round > 1)
+            {
+                Player.showitems();
+                attempt = Console.ReadLine();
+                guess(word, attempt);
+            }
+            else
+            {
+                guess(word, attempt);
+            }
+        }
+        public static void guess(string word, string guess)
+        {
+            for(int i = 0; i < word.Length; i++)
+            {
+                if(i < guess.Length)
+                {
+                    if (guess[i]== word[i])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        dealt++;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        recieved++;
+                    }
+                    Console.Write(guess[i]);
+                }
+                else
+                {
+                    recieved++;
+                }
+                Console.ResetColor();
+            }
+            for (int i = word.Length; i < guess.Length; i++)
+            {
+                recieved++;
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.Write(guess[i]);
+                Console.ResetColor();
+            }
+            if (Player.difference)
+            {
+                dealt -= recieved;
+                if(dealt > 0)
+                {
+                    updatehp(dealt);
+                }
+                else
+                {
+                    Player.updatehp(dealt);
+                }
+                dealt = 0;
+                recieved = 0;
+            }
+            else
+            {
+                updatehp(dealt);
+                Player.updatehp(recieved);
+                dealt = 0;
+                recieved = 0;
+            }
+            Console.WriteLine();
+        }
+        internal static void updatehp(int damage)
+        {
+            if (Player.doubler)
+            {
+                damage *= 2;
+            }
+            hp -= damage;
+            if (hp <= 0)
+            {
+                Game.complete();
+            }
+        }
+
+    }
+}
---
+++ Rooms/Room.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemp.Rooms
+{
+    internal abstract class Room
+    {
+        internal abstract void introduction(bool[] beatR, bool[] beatL);
+        internal static void complete(bool[] beatR, int level, int round)
+        {
+            string[] parts = { "arms", "legs", "head+torso" };
+            if(level != 5)
+            {
+                Console.WriteLine($"\nCongrats on beating beast {level}, it only gets harder from here." +
+    "\nWould you like to [Continue] to the next room" +
+    "\n or stop to [Admire] the room some more");
+            }
+            else
+            {
+                Console.WriteLine($"\nWow!! you beat boss {round}/3, Congrats. Now you can choose One item from the shop");
+                Collectables.shop();
+            }
+            string choice = Console.ReadLine();
+            if (!beatR[0])
+            {
+                switch (choice)
+                {
+                    case "Continue":
+                        Console.WriteLine("Wait, before you go, dont forget to [Collect] the arms");
+                        Console.ReadLine();
+                        break;
+                    case "Admire":
+                        Console.WriteLine("The walls haven't changed. Still full of letters. Would you like to [Sit] for a moment and look for words in the walls?");
+                        choice = Console.ReadLine();
+                        if (choice == "Sit")
+                        {
+                            //start 15 second timer
+                            Console.WriteLine("YOU FOUND SOMETHING!!!" +
+                                "\nPress enter to read it");
+                            Console.ReadLine();
+                            switch (level)
+                            {
+                                case 1:
+                                    Console.WriteLine("The word reads \"gullible\"");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("The word reads \"GuLlIbLe\"");
+                                    break;
+                                case 3:
+                                    Console.WriteLine("The word reads \"Gu1LibL3\"");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("The word reads \"gullible\"");
+                                    Console.WriteLine("Weird... this beast never used letters before now");
+                                    break;
+                                case 5:
+                                    Console.WriteLine("The word reads \"Gu1L!bL3\"");
+                                    break;
+                            }
+                            Console.WriteLine("You feel like a fool, more than ever before");
+                            Console.WriteLine("Before you leave, don't forget to [Collect] the beasts arms");
+                            Console.ReadLine();
+                        }
+                        break;
+                }
+            }
+            else if (!beatR[1])
+            {
+                switch (choice)
+                {
+                    case "Continue":
+                        Console.WriteLine("Wait, before you go, dont forget to [Collect] the legs");
+                        Console.ReadLine();
+                        break;
+                    case "Admire":
+                        Console.WriteLine("The walls haven't changed. Still full of letters. Don't waste anymore time here");
+                        Console.WriteLine("Before you leave, don't forget to [Collect] the beasts legs");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            else
+            {
+                switch (choice)
+                {
+                    case "Continue":
+                        Console.WriteLine("You've been through this... you need to [Collect] the torso and head");
+                        Console.ReadLine();
+                        break;
+                    case "Admire":
+                        Console.WriteLine("The walls haven't changed. Still full of letters. Don't waste anymore time here");
+                        Console.WriteLine("Before you leave, don't forget to [Collect] the beasts legs");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            Console.WriteLine("You've collected [" + parts[round - 1] + level + "]");
+            Player.PartAdd(parts[round - 1] + level);
+            Player.ShowInventory();
+            Console.WriteLine("[Continue] to the next room");
+            Console.ReadLine();
+        }
+    }
+}
