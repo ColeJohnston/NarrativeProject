569d556fbb8cda978191755fd98775b4f689db4a
Cole Johnston <Cole.e.johnston16@gmail.com>
2024-04-21 22:54:34 -0400

    Added Program file
    This file is to store the Main function

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ProjectTemp;
+using ProjectTemp.Enemies;
+using ProjectTemp.Rooms;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //character select
+        //introduce story
+        //begin room one
+        Player player = new Player();
+        player.CharacterSelect();
+        player.SetHp();
+        Fighting.StartFight();
+        while (Game.round < 4)
+        {
+            Fighting.Fight();
+        }
+    }
+}
